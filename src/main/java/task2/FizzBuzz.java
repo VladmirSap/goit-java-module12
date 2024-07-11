fbcc2f8c0202b6731111973abca448cc2fb5b604
VladmirSap <107324463+VladmirSap@users.noreply.github.com>
2024-07-12 01:22:53 +0300

    Commit2

--- src/main/java/task2/FizzBuzz.java
+++ src/main/java/task2/FizzBuzz.java
@@ -1,83 +1,90 @@
 package task2;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class FizzBuzz {
-    private final int n;
+    private int n;
     private int current = 1;
-    private final Object lock = new Object();
+    private CyclicBarrier barrier = new CyclicBarrier(4);
 
     public FizzBuzz(int n) {
         this.n = n;
     }
 
     public void fizz() {
-        while (true) {
-            synchronized (lock) {
-                while (current <= n && (current % 3 != 0 || current % 5 == 0)) {
-                    try {
-                        lock.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (current > n) return;
-                System.out.println("fizz");
-                current++;
-                lock.notifyAll();
+        while (current <= n) {
+            if (current % 3 == 0 && current % 5 != 0) {
+                printFizz();
+            } else {
+                awaitBarrier();
             }
         }
     }
 
     public void buzz() {
-        while (true) {
-            synchronized (lock) {
-                while (current <= n && (current % 5 != 0 || current % 3 == 0)) {
-                    try {
-                        lock.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (current > n) return;
-                System.out.println("buzz");
-                current++;
-                lock.notifyAll();
+        while (current <= n) {
+            if (current % 5 == 0 && current % 3 != 0) {
+                printBuzz();
+            } else {
+                awaitBarrier();
             }
         }
     }
 
     public void fizzbuzz() {
-        while (true) {
-            synchronized (lock) {
-                while (current <= n && (current % 3 != 0 || current % 5 != 0)) {
-                    try {
-                        lock.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (current > n) return;
-                System.out.println("fizzbuzz");
-                current++;
-                lock.notifyAll();
+        while (current <= n) {
+            if (current % 3 == 0 && current % 5 == 0) {
+                printFizzBuzz();
+            } else {
+                awaitBarrier();
             }
         }
     }
 
     public void number() {
-        while (true) {
-            synchronized (lock) {
-                while (current <= n && (current % 3 == 0 || current % 5 == 0)) {
-                    try {
-                        lock.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (current > n) return;
-                System.out.println(current);
-                current++;
-                lock.notifyAll();
+        while (current <= n) {
+            if (current % 3 != 0 && current % 5 != 0) {
+                printNumber();
+            } else {
+                awaitBarrier();
             }
+        }
+    }
+
+    private void printFizz() {
+        synchronized (this) {
+            System.out.println("fizz");
+            current++;
+        }
+    }
+
+    private void printBuzz() {
+        synchronized (this) {
+            System.out.println("buzz");
+            current++;
+        }
+    }
+
+    private void printFizzBuzz() {
+        synchronized (this) {
+            System.out.println("fizzbuzz");
+            current++;
+        }
+    }
+
+    private void printNumber() {
+        synchronized (this) {
+            System.out.println(current);
+            current++;
+        }
+    }
+
+    private void awaitBarrier() {
+        try {
+            barrier.await(); // Wait for other threads
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
         }
     }
 }
